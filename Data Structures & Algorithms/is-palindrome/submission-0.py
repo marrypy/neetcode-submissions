72c9bc2478a05ed9a98960280b86f5e65b895e51
Marry Kassa <marry.kassa@yale.edu>
2026-07-12 02:21:12 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ""
+
+        for c in s:
+            if c.isalnum():
+                cleaned += c.lower()
+
+        left = 0
+        right = len(cleaned) - 1
+
+        while left < right:
+            if cleaned[left] != cleaned[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
